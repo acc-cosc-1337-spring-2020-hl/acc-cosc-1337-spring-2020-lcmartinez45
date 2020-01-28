8fa6c6cc69ee9a885e7243a4ba08d5762452d510
lcmartinez45 <lillian.martinez@g.austincc.edu>
2020-01-27 19:07:58 -0600

    Void and return functions with unit test

--- src/examples/01_module/02_variables/main.cpp
+++ src/examples/01_module/02_variables/main.cpp
@@ -1,6 +1,15 @@
+#include "vars.h"
 
 int main() 
 {
+	display_number(10);
+
+	int num = 5;
+	cout << square_number(num) << "\n";
+
+	cout << num << "\n";
+
+	int num1 = square_number(5);
 
 	return 0;
 }
