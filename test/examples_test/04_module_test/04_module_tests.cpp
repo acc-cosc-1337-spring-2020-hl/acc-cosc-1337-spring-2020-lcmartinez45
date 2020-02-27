92003e614fa71a4f5e783da608a2c052002d3872
lcmartinez45 <lillian.martinez@g.austincc.edu>
2020-02-26 19:12:25 -0600

    in class assignment

--- test/examples_test/04_module_test/04_module_tests.cpp
+++ test/examples_test/04_module_test/04_module_tests.cpp
@@ -5,3 +5,27 @@ TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE("Test BankAccount get_balance") {
+
+}
+
+TEST_CASE("Test BankAccount deposit") {
+	REQUIRE(account.get_balance() == 150);
+	REQUIRE_THROWS_AS(account.deposit(-50).InvalidAmount);
+
+	REQUIRE(account.get_balance == 150);
+	account.deposit(50);
+}
+
+TEST_CASE("Test BankAccount withdraw") {
+	BankAccount account(150)
+	REQUIRE_THROWS_AS(account.withdraw(-5), InvalidAmount);
+	REQUIRE(account.get_balance() == 150);
+
+	account.withdraw(50);
+	REQUIRE(account.get_balance() == 100);
+
+	REQUIRE_THROWS_AS(account.withdraw(101), InvalidAmount);
+	REQUIRE(account.get_balance() == 100);
+
+}
