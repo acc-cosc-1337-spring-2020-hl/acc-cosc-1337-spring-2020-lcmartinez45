0589c2579cfc8ae6e296c329aaaceefbc5421ae9
Lillian Martinez <lillian.martinez@g.austincc.edu>
2020-03-05 20:43:59 -0600

    homework

--- src/homework/tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/tic_tac_toe/tic_tac_toe.cpp
@@ -1,1 +1,47 @@
-//cpp
+//tic_tac_toe.cpp
+#include "tic_tac_toe.h"
+
+/*
+ 1) first_player function argument value must be X or O; otherwise, throw an Error exception when value is not X or O.  Error Message: Player must be X or O.
+ 2)In function set player(private variable) to first_player function argument.
+ */
+void TicTacToe::start_game(string first_player)
+{
+  if(first_player != "O" || first_player != "X")
+  {
+    throw Error("Player must be X or O.");
+    
+  }else{
+    player = first_player;
+  }
+}
+
+/*
+ 1) Value of int must be in the range 1 to 9; otherwise, throw an Error exception if value not in this range. Error Message: Position must be 1 to 9.
+ 2) Private data player can't be empty "", throw an Error exception if player variable is "". Error Message: Must start game first.
+ 3) Call set_next_player private function
+ */
+void TicTacToe::mark_board(int position)
+{
+  if (position > 9 && position < 1)
+  {
+    throw Error("\nPosition must be 1 to 9.");
+  }
+
+  if (player == "")
+  {
+    throw Error("\nMust start game first.");
+  }
+  set_next_player();
+}
+
+void TicTacToe::set_next_player()
+{
+  if(player == "X")
+  {
+    next_player = "O";
+  }
+  else{
+    next_player = "X";
+  }
+}
--- src/homework/tic_tac_toe/tic_tac_toe.h
+++ src/homework/tic_tac_toe/tic_tac_toe.h
@@ -1,1 +1,42 @@
-//h
+//tic_tac_toe.h
+#include<string>
+#include<iostream>
+
+using std::string;
+
+#ifndef TIC_TAC_TOE_H
+#define TIC_TAC_TOE_H
+class TicTacToe
+{
+public:
+  void start_game(string first_player);
+  void mark_board(int position);
+   
+  // Return the next_player value
+  string get_player()const { return next_player; }
+  
+private:
+  string next_player;
+   
+  //Class member variable
+  string player;
+   
+  //Set next_player. If private variable player X, player is O else player is X
+  void set_next_player();
+};
+
+class Error
+{
+public:
+  //Constructor: initialized private variable message with function parameter msg
+  Error(string msg) : message{ msg } {};
+   
+  //Returns the message private variable
+  string get_message()const { return message; }
+
+private:
+  //Class member variable
+  string message;
+};
+
+#endif // !TIC_TAC_TOE_H
