f7275f91b7651738e9923035e7f4cb6d3e97449e
Lillian Martinez <lillian.martinez@g.austincc.edu>
2020-04-20 18:55:08 -0500

    vector assignment

--- src/classwork/11_assign/vector.h
+++ src/classwork/11_assign/vector.h
@@ -1,1 +1,18 @@
 //
+#ifndef MY_VECTOR_H
+#define MY_VECTOR_H
+//Rule of 3, taking the role of memory management we have to take a lot of overhead
+class Vector
+{
+public:
+    Vector(size_t sz);
+    Vector(const Vector& v); //copy constructor
+    size_t Size()const { return size; }
+    int& operator[] (int i){ return nums[i]; }
+    int& operator[] (int i) const { return nums[i]; }
+
+private:
+    size_t size;
+    int* nums;
+};
+#endif
