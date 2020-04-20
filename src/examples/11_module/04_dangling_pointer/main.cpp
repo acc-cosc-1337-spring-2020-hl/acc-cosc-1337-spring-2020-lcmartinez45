1cdf792e25af04b48155feee1cdbb4fcb5f49bf2
Lillian Martinez <lillian.martinez@g.austincc.edu>
2020-04-20 17:58:55 -0500

    dangling pointer

--- src/examples/11_module/04_dangling_pointer/main.cpp
+++ src/examples/11_module/04_dangling_pointer/main.cpp
@@ -1,8 +1,15 @@
 #include "dangling_pointer.h"
+#include <iostream>
+using std::cout;
 
 int main() 
 {
-	
+    int* ptr_num = new int(5);
+    int* ptr = ptr_num;
+    cout << *ptr << "\n";
+    delete ptr_num;
+    cout << *ptr << "\n";
+    ptr_num = nullptr;
 
 	return 0;
 }
